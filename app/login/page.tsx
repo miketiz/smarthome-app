61ec1da3ecc3df0a643e1d0ca5264a6e5d7a5b45
Poramet.mo <mikeclassic1012@hotmail.com>
2026-02-11 14:26:20 +0700

    loginpage

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { useState } from "react"
+
+export default function LoginPage() {
+    const[userName,setUserName]=useState("")
+    const[passWord,setPassWord]=useState("")
+    
+    return(
+        <div></div>
+    )
+}
